cdf29c122b9d633479e625dca75106b7f77201f0
zevs-x <bludenovsasa@gmail.com>
2025-12-02 15:11:35 +0700

    сделана админ но ток начало

---
+++ admin_panel/login_admin.php
@@ -0,0 +1,20 @@
+<?php //Регестрация для админа
+session_start();
+require_once 'resource/connect.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Тестирование профессий</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+
+    
+</body>
+</html>
