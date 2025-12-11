a1d7e94d522069601bfbb5d3899272b0ac99ad23
zevs-x <bludenovsasa@gmail.com>
2025-12-11 21:48:33 +0700

    y

--- admin_panel/home_admine.php
+++ admin_panel/home_admine.php
@@ -9,6 +9,7 @@ require_once '../resource/connect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Админ панель</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
