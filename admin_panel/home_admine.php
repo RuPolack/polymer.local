7c04d6ed9f8ce48e1ae3a3223ecce8f95b088f3f
zevs-x <bludenovsasa@gmail.com>
2025-12-11 21:32:04 +0700

    fd

--- admin_panel/home_admine.php
+++ admin_panel/home_admine.php
@@ -8,45 +8,45 @@ require_once '../resource/connect.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Админ панель</title>
 </head>
 <body>
 
     <div>
-        <form id="admin" action="../admin_panel/.." method="GET">
+        <form id="1" action="../admin_panel/actions/admin_help.php" method="GET">
             <button type="submit" class="btn">Как пользоваться админом</button>
         </form>
     </div>
-
+ 
     <div>
-        <form id="admin" action="../admin_panel/.." method="GET">
+        <form id="2" action="../admin_panel/actions/view_results.php" method="GET">
             <button type="submit" class="btn">Просмотр результатов</button>
         </form>
     </div>
 
     <div>
-        <form id="admin" action="../admin_panel/change/change_password.php" method="GET">
+        <form id="3" action="../admin_panel/change/change_password.php" method="GET">
             <button type="submit" class="btn">Поменять пароль</button>
         </form>
-        <form id="admin" action="../admin_panel/change/change_name.php" method="GET">
+        <form id="4" action="../admin_panel/change/change_name.php" method="GET">
             <button type="submit" class="btn">Поменять имя</button>
         </form>
     </div>
 
     <div>
-        <form id="admin" action="../admin_panel/.." method="GET">
+        <form id="5" action="../admin_panel/actions/add_question.php" method="GET">
             <button type="submit" class="btn">Добавить вопрос</button>
         </form>
-        <form id="admin" action="../admin_panel/.." method="GET">
+        <form id="6" action="../admin_panel/actions/edit_question.php" method="GET">
             <button type="submit" class="btn">Редактировать вопрос</button>
         </form>
     </div>
 
     <div>
-        <form id="admin" action="../admin_panel/.." method="GET">
+        <form id="7" action="../admin_panel/actions/add_profession.php" method="GET">
             <button type="submit" class="btn">Добавить профессию</button>
         </form>
-        <form id="admin" action="../admin_panel/.." method="GET">
+        <form id="8" action="../admin_panel/actions/edit_profession.php" method="GET">
             <button type="submit" class="btn">Редактировать профессию</button>
         </form>
     </div>
